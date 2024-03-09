801d6f66c0e6277893e476863fecf603a03d6ad4
5win <94297900+5win@users.noreply.github.com>
2024-03-09 21:28:46 +0900

    [Silver II] Title: 종이의 개수, Time: 360 ms, Memory: 37200 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,72 +1,62 @@
-#include<iostream>
-#include<string>
-#include<cstring>
-#include<vector>
-#include<cmath>
-#include<sstream>
-#include<limits>
-#include<algorithm>
-#include<stack>
-#include<list>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string.h>
 
 using namespace std;
-const int INF = 1987654321;
-const int MAX = 101;
-const int _MOD = 1000000007;
 
-int n, k;
-int arr[2200][2200];
-int ret[3];
+int n;
+int board[3001][3001];
+int res[3];
+const int NOTPAPER = -2;
 
-bool canUse(int y, int x, int size) {
-    int num = arr[y][x];
+int canPaper(int y, int x, int len) {
+    int num = board[y][x];
 
-    for(int i = y; i < y + size; i++) 
-        for(int j = x; j < x + size; j++) 
-            if(arr[i][j] != num) return false;
-    return true;
+    int cnt = 0;
+    for(int i = 0; i < len; i++) {
+        for(int j = 0; j < len; j++) {
+            if(board[y + i][x + j] == num) cnt++;
+            else return NOTPAPER;
+        }
+    }
+    if(cnt == len * len) {
+        return num;
+    }
+    return NOTPAPER;
 }
 
-void solve(int y, int x, int size) {
-    if(size == 1) {
-        if(arr[y][x] == -1) ret[0]++;
-        else if(arr[y][x] == 0) ret[1]++;
-        else ret[2]++;
+void dfs(int y, int x, int len) {
+
+    int pNum = canPaper(y, x, len);
+    if(pNum != NOTPAPER) {
+        res[pNum + 1]++;
         return;
     }
 
-    if(canUse(y, x, size)) {
-        if(arr[y][x] == -1) ret[0]++;
-        else if(arr[y][x] == 0) ret[1]++;
-        else ret[2]++;
-        return;
-    }
-    
-    int len = size / 3;
+    int divLen = len / 3;
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
-            solve(y + i * len, x + j * len, len);
+            dfs(y + i * divLen, x + j * divLen, divLen);
         }
     }
-    return;    
 }
 
-int main(void)
-{
-    cin.tie(NULL);
+int main(void) {
+cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-
     cin >> n;
 
-    for(int i = 0 ; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            cin >> arr[i][j];
+            cin >> board[i][j];
         }
     }
-    solve(0, 0, n);
+
+    dfs(0, 0, n);
 
     for(int i = 0; i < 3; i++)
-        cout << ret[i] << endl;
+        cout << res[i] << endl;
 
     return 0;
 }
