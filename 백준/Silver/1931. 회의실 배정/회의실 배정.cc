9ccf2bdee9c27d99d3e4590fb830c300dbfe1b20
5win <94297900+5win@users.noreply.github.com>
2024-03-18 14:57:22 +0900

    [Silver I] Title: 회의실 배정, Time: 88 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(void) {
+
+    int n;
+    vector<pair<int, int>> v;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int s, e;
+        cin >> s >> e;
+        v.push_back({s, e});
+    }
+
+    sort(v.begin(), v.end());
+
+    int cnt = 1;
+    pair<int, int> prev = v[0];
+    for(int i = 1; i < v.size(); i++) {
+        pair<int, int> cur = v[i];
+        if(cur.second < prev.second) {
+            prev = cur;
+            // if(prev.second == cur.first)
+            //     cnt++;
+        }
+        else if(cur.first >= prev.second) {
+            prev = cur;
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
