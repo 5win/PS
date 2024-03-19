0327d5b52854fe7317bf87b987adda1ea3b22edb
5win <94297900+5win@users.noreply.github.com>
2024-03-19 15:07:14 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -10,30 +10,18 @@ using namespace std;
 int n;
 int cache[20];
 
-int dfs(int curNum) {
-
-    if(curNum < 0) return 0;
-    else if(curNum == 0) return 1;
-
-    int &ret = cache[curNum];
-    if(ret != -1)
-        return ret;
-
-    int cnt = 0;
-    cnt += dfs(curNum - 1);
-    cnt += dfs(curNum - 2);
-    cnt += dfs(curNum - 3);
-
-    return ret = cnt;
-}
-
 int main(void) {
+
+    cache[0] = 1;
+    cache[1] = 1;
+    cache[2] = 2;
+    for(int i = 3; i <= 12; i++) 
+        cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3];
 
     int t; cin >> t;
     while(t--) {
-        memset(cache, -1, sizeof(cache));
         cin >> n;
-        cout << dfs(n) << endl;
+        cout << cache[n] << endl;
     }
 
     return 0;
