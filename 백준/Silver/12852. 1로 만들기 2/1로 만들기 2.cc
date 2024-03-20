aa50388baf8a251baf6250a0a87e01e5dd2be73c
5win <94297900+5win@users.noreply.github.com>
2024-03-20 11:30:27 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n;
+int dp[1000001];
+int trace[1000001];
+
+int main(void) {
+
+    cin >> n;
+
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    trace[2] = 1;
+    trace[3] = 1;
+
+    for(int i = 4; i <= n; i++) {
+        int mn = INT_MAX;
+        if(i % 3 == 0 && mn > dp[i / 3]) {
+            dp[i] = dp[i / 3];
+            trace[i] = i / 3;
+            mn = dp[i / 3];
+        }
+        if(i % 2 == 0 && mn > dp[i / 2]) {
+            dp[i] = dp[i / 2];
+            trace[i] = i / 2;
+            mn = dp[i / 2];
+        }
+        if(mn > dp[i - 1]) {
+            dp[i] = dp[i - 1];
+            trace[i] = i - 1;
+            mn = dp[i - 1];
+        }
+        dp[i] += 1;
+    }
+
+    cout << dp[n] << endl;
+    cout << n << ' ';
+    while(n > 1) {
+        n = trace[n];
+        cout << n << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
