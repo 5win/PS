c9ab4caaf0272bdbf840563b481a12051bab0df2
5win <94297900+5win@users.noreply.github.com>
2024-03-25 23:16:07 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<cmath>
+#include<sstream>
+#include<limits>
+#include<algorithm>
+#include<stack>
+#include<list>
+#include<queue>
+#include<deque>
+#include<map>
+#include<numeric>
+
+#define endl '\n'
+
+using namespace std;
+const int INF = 1987654321;
+const int MAX = 1000 * 1000 * 10;
+const int _MOD = 1000000007;
+
+int n;
+int m;
+int idx = 0;
+int arr[10];
+vector<int> v;
+
+void solve(int start, int len) {
+    if(len == m) {
+        for(int i = 0; i < v.size(); i++)
+            cout << v[i] << ' ';
+        cout << endl;
+        return;
+    }
+
+    int prev = -1;
+    for(int i = start; i < n; i++) {
+        if(arr[i] != prev) {
+            v.push_back(arr[i]);
+            solve(i + 1, len + 1);
+            v.pop_back();
+            prev = arr[i];
+        }
+    }
+}
+
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) 
+        cin >> arr[i];
+    sort(arr, arr + n);
+
+    solve(0, 0);
+    
+    return 0;
+}
