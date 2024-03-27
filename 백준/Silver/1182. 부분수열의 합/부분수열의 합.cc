c1ed5926d52548e337d9505820dec851e18d456e
5win <94297900+5win@users.noreply.github.com>
2024-03-28 00:41:33 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,37 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <string.h>
-#include <algorithm>
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<cmath>
+#include<sstream>
+#include<limits>
+#include<algorithm>
+#include<stack>
+#include<list>
+#include<queue>
+#include<deque>
+#include<map>
+#include<numeric>
+
+#define endl '\n'
 
 using namespace std;
+const int INF = 1987654321;
+const int MAX = 1000 * 1000 * 10;
+const int _MOD = 1000000007;
 
-int n, s;
-int cnt;
-vector<int> v;
+int n;
+int s;
+int arr[21];
+int cnt = 0;
 
-void dfs(int len, int sum, int count) {
-
-    if(len == n) {
-        if(sum == s && count)
-            cnt++;
-        return;
+void solve(int start, int sum) {
+    if(sum == s) {
+        cnt++;
     }
-
-    dfs(len + 1, sum + v[len], count + 1);
-    dfs(len + 1, sum, count);
+    for(int i = start + 1; i < n; i++) {
+         solve(i, sum + arr[i]);
+    }
+    return;
 }
 
-int main(void) {
-
+int main(void)
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
     cin >> n >> s;
-    for(int i = 0; i < n; i++) {
-        int tmp; cin >> tmp;
-        v.push_back(tmp);
-    }
-   
-    dfs(0, 0, 0);
+    for(int i = 0; i < n; i++) 
+        cin >> arr[i];
 
+    for(int i = 0; i < n; i++) {
+        solve(i, arr[i]);
+    }
     cout << cnt << endl;
 
     return 0;
