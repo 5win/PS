b18fa1dcaba16caa0938cee27fa13b12acb841d7
5win <94297900+5win@users.noreply.github.com>
2024-03-27 10:40:22 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -11,37 +11,54 @@
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 using namespace std;
 
+const int INF = 1987654321;
+
 int n;
-vector<pair<double, double>> vertex;
-vector<pair<double, pair<double, double>>> edges;
-vector<int> parent(101);
+vector<pair<int, int>> vertex;
+vector<pair<int, double>> adj[101];
+vector<bool> visited(101, false);
+vector<double> minWeight(101, INF);
 
 void calcEdge() {
     for(int i = 0; i < vertex.size(); i++) {
         for(int j = i + 1; j < vertex.size(); j++) {
             double cost = sqrt(pow(vertex[i].first - vertex[j].first, 2) + pow(vertex[i].second - vertex[j].second, 2));
-            edges.push_back({cost, {i, j}});
+            adj[i].push_back({j, cost});
+            adj[j].push_back({i, cost});
         }
     }
 }
+            
+double prim() {
 
-int find(int u) {
-    if(u == parent[u]) return u;
-    return parent[u] = find(parent[u]);
-}
+    double ret = 0;
 
-void merge(int u, int v) {
-    u = find(u); v = find(v);
-    if(u == v) return;
-    parent[u] = v;
+    minWeight[0] = 0;
+
+    for(int iter = 0; iter < n; iter++) {
+        int u = -1;         // 다음에 추가할 정점
+        for(int i = 0; i < n; i++) {
+            // 추가하지 않은 정점이면서 && (첫 정점을 선택하는 경우이거나 || 더 작은 정점이 있는 경우)
+            if(!visited[i] && (u == -1 || minWeight[u] > minWeight[i]))
+                u = i;
+        }
+        ret += minWeight[u];
+        visited[u] = true;
+
+        for(int i = 0; i < adj[u].size(); i++) {
+            int nextV = adj[u][i].first;
+            double cost = adj[u][i].second;
+            if(visited[nextV] || minWeight[nextV] <= cost) continue;
+            minWeight[nextV] = cost;
+        }
+    }
+    return ret;
 }
 
 int main(void) {
     FASTIO;
 
     cin >> n;
-    for(int i = 0; i < n; i++)
-        parent[i] = i;
 
     double x, y;
     for(int i = 0; i < n; i++) {
@@ -50,18 +67,8 @@ int main(void) {
     }
     calcEdge();
 
-    sort(edges.begin(), edges.end());
-
-    double sum = 0;
-    for(int i = 0; i < edges.size(); i++) {
-        int u = edges[i].second.first, v = edges[i].second.second;
-        double cost = edges[i].first;
-        if(find(u) == find(v)) continue;
-        merge(u, v);
-        sum += cost;
-    }
     cout.precision(3);
-    cout << sum << endl;
+    cout << prim() << endl;
 
     return 0;
 }
