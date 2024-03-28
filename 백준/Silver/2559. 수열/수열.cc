975041e6b9c5aac7be7b80d99404a78d5edeaffc
5win <94297900+5win@users.noreply.github.com>
2024-03-28 13:51:39 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -12,22 +12,25 @@ const int INF = 1987654321;
 
 int n, k;
 int arr[100001];
-int psum[100001];
 
 int main(void) {
     FASTIO;
 
     cin >> n >> k;
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i < n; i++)
         cin >> arr[i];
-
-    for(int i = 0; i <= n; i++)
-        psum[i] = psum[i - 1] + arr[i];
-
-    int res = -INF;
-    for(int i = 1; i <= n - k + 1; i++) {
-        res = max(res, psum[i + k - 1] - psum[i - 1]);
-    }    
+    
+    int s = 0, e = k;
+    int sum = 0;
+    for(int i = 0; i < e; i++)
+        sum += arr[i];
+    
+    int res = sum;
+    while(e < n) {
+        sum = sum - arr[s] + arr[e];
+        res = max(res, sum);
+        s++; e++;
+    }
     cout << res << endl;
 
     return 0;
