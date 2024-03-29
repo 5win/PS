b0e8f5187dd4f8c6084ce9eb22756b5a42e93564
5win <94297900+5win@users.noreply.github.com>
2024-03-29 17:10:02 +0900

    [Gold III] Title: 채굴, Time: 664 ms, Memory: 7048 KB -BaekjoonHub

---
+++ 백준/Gold/15573. 채굴/채굴.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const int INF = 1987654321;
+
+int n, m, k;
+int board[1001][1001];
+bool visited[1001][1001];
+queue<pair<int, int>> q;
+
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
+
+int calcSum() {
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            if(visited[i][j]) cnt++;
+    return cnt;
+}
+
+bool inRange(int y, int x) {
+    return 0 <= y && y < n && 0 <= x && x < m;
+}
+
+void bfs(int y, int x, int d) {
+    // queue<pair<int, int>> q;
+    // q.push({y, x});
+    // visited[y][x] = true;
+
+    while(!q.empty()) {
+        pair<int, int> cur = q.front();
+        q.pop();
+
+        y = cur.first; x = cur.second;
+        for(int dir = 0; dir < 4; dir++) {
+            int ny = y + dy[dir], nx = x + dx[dir];
+            if(!inRange(ny, nx) || board[ny][nx] > d || visited[ny][nx]) continue;
+            visited[ny][nx] = true;
+            q.push({ny, nx});
+        }
+    }
+}
+
+int canBfs(int d) {
+
+    while(!q.empty()) {
+        q.pop();
+    }
+
+    memset(visited, 0, sizeof(visited));
+
+    for(int i = 0; i < n; i++) {
+        if(board[i][0] <= d && !visited[i][0]) {
+            q.push({i, 0});
+            visited[i][0] = true;
+        }
+            // bfs(i, 0, d);
+        if(board[i][m - 1] <= d && !visited[i][m - 1]) {
+            q.push({i, m - 1});
+            visited[i][m - 1] = true;
+        }
+            // bfs(i, m - 1, d);
+    }
+    for(int i = 0; i < m; i++) {
+        if(board[0][i] <= d && !visited[0][i]) {
+            q.push({0, i});
+            visited[0][i] = true;
+        }
+            // bfs(0, i, d);
+    }
+    bfs(0, 0, d);
+
+    return calcSum();
+}
+
+int binarySearch(int lo, int hi) {
+
+    while(lo < hi) {
+        int mid = (lo + hi) / 2;
+        int cnt = canBfs(mid);
+        
+        if(cnt >= k) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    return lo;
+}
+
+int main(void) {
+    FASTIO;
+
+    cin >> n >> m >> k;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    cout << binarySearch(1, 1e6) << '\n';
+        
+    return 0;
+}
