149250812a244f0724f9b92ab51fbacb93aca57e
5win <94297900+5win@users.noreply.github.com>
2024-04-03 14:21:22 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 5204 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const int INF = 1987654321;
+
+int n;
+vector<pair<int, int>> adj[100001];
+bool visited[100001];
+int res;
+
+int dfs(int cur, int cost) {
+    int mx = 0;
+    int ret = 0;
+
+    for(int i = 0; i < adj[cur].size(); i++) {
+        if(visited[adj[cur][i].first]) continue;
+        visited[adj[cur][i].first] = true;
+        ret = dfs(adj[cur][i].first, adj[cur][i].second);
+        res = max(res, ret + mx);
+        mx = max(mx, ret);
+    }
+    return mx + cost;
+}
+
+int main(void) {
+    FASTIO;
+
+    cin >> n;
+    for(int i = 0; i < n - 1; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+    visited[1] = true;
+    dfs(1, 0);
+    cout << res << '\n';
+    
+    return 0;
+}   
