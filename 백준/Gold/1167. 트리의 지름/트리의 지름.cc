27fd4896c8350f3c353c73095935d4071dd0c0a5
5win <94297900+5win@users.noreply.github.com>
2024-04-03 14:04:09 +0900

    [Gold II] Title: 트리의 지름, Time: 72 ms, Memory: 8644 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -13,29 +13,21 @@ const int INF = 1987654321;
 
 int n;
 vector<pair<int, int>> adj[100001];
-int visited[100001];
+bool visited[100001];
+int res;
 
-int bfs(int start) {
-    memset(visited, 0, sizeof(visited));
-    queue<int> q;
-    q.push(start);
-    visited[start] = 1;
+int dfs(int cur, int cost) {
+    int mx = 0;
+    int ret = 0;
 
-    int ret = 1;
-    while(!q.empty()) {
-        int cur = q.front();
-        q.pop();
-        for(int i = 0; i < adj[cur].size(); i++) {
-            int nNode = adj[cur][i].first;
-            int nWeight = adj[cur][i].second;
-            if(visited[nNode]) continue;
-            visited[nNode] = visited[cur] + nWeight;
-            q.push(nNode);
-
-            ret = (visited[ret] > visited[nNode]) ? ret : nNode;
-        }
+    for(int i = 0; i < adj[cur].size(); i++) {
+        if(visited[adj[cur][i].first]) continue;
+        visited[adj[cur][i].first] = true;
+        ret = dfs(adj[cur][i].first, adj[cur][i].second);
+        res = max(res, ret + mx);
+        mx = max(mx, ret);
     }
-    return ret;
+    return mx + cost;
 }
 
 int main(void) {
@@ -46,16 +38,13 @@ int main(void) {
         int u, v, w;
         cin >> u;
         while(true) {
-            cin >> v;
-            if(v == -1) break;
-            cin >> w;
-            adj[u].push_back({v, w});
+            cin >> v;   if(v == -1) break;
+            cin >> w;   adj[u].push_back({v, w});
         }
     }
-    // cout << visited[bfs(bfs(1))] - 1 << '\n';
-    int maxDistV = bfs(1);
-    maxDistV = bfs(maxDistV);
-    cout << visited[maxDistV] - 1 << '\n';
+    visited[1] = true;
+    dfs(1, 0);
+    cout << res << '\n';
     
     return 0;
 }   
