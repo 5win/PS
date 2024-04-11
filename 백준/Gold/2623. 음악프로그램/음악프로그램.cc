fcbed019d8e7800e6206b12e9ed5ce0835280d95
5win <94297900+5win@users.noreply.github.com>
2024-04-12 00:50:15 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -0,0 +1,73 @@
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+
+const int INF = 1987654321;
+const int MOD = 1000000007;
+
+int n, m;
+int indgree[1001];
+vector<int> adj[1001];
+vector<int> ans;
+
+bool topologySort() {
+    queue<int> q;
+    for(int i = 1; i <= n; i++) {
+        if(indgree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        ans.push_back(cur);
+        indgree[cur]--;
+
+        for(int i = 0; i < adj[cur].size(); i++) {
+            int next = adj[cur][i];
+            indgree[next]--;
+            if(indgree[next] == 0) {
+                q.push(next);
+            } else if(indgree[next] < 0) {
+                return false;
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++)
+        if(indgree[i] > 0) return false;
+    return true;
+}
+
+int main(void) {
+    FASTIO;
+
+    cin >> n >> m;
+    for(int i = 0; i < m; i++) {
+        int k; cin >> k;
+        int a, b;
+        cin >> a;
+        for(int j = 0; j < k - 1; j++) {
+            cin >> b;
+            adj[a].push_back(b);
+            indgree[b]++;
+            a = b;
+        }
+    }
+    if(topologySort())
+        for(auto& i : ans) cout << i << '\n';
+    else
+        cout << "0\n"; 
+
+    return 0;
+}
