e8b17380f9057562d20caed1ec329b1968663f7e
5win <94297900+5win@users.noreply.github.com>
2024-04-19 17:10:00 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2332 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -14,28 +14,43 @@ using namespace std;
 const int INF = 1987654321;
 const int MOD = 1000000007;
 
-vector<int> lec[10001];
+int parent[10001];
+vector<pair<int, int>> lec;
+
+int find(int u) {
+    if(parent[u] == u) return u;
+    return parent[u] = find(parent[u]);
+}
+
+void merge(int u, int v) {
+    u = find(u); v = find(v);
+    if(u == v) return;
+    parent[u] = v;
+}
+
+bool comp(const pair<int, int>& a, const pair<int, int>& b) {
+    if(a.first == b.first) return a.second < b.second;
+    return a.first > b.first;
+}
 
 int main(void) {
     FASTIO;
 
-    int n, p, d, maxDate = 0;
+    int n, p, d;
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> p >> d;
-        lec[d].push_back(p);
-        maxDate = max(maxDate, d);
+        lec.push_back({p, d});
     }
+    sort(lec.begin(), lec.end(), comp);
+    for(int i = 0; i <= 10000; i++)
+        parent[i] = i;
 
     int ans = 0;
-    priority_queue<int> pq;
-    for(int i = maxDate; i > 0; i--) {
-        for(int j = 0; j < lec[i].size(); j++)
-            pq.push(lec[i][j]);
-        if(!pq.empty()) {
-            ans += pq.top(); 
-            pq.pop();
-        }
+    for(int i = 0; i < lec.size(); i++) {
+        if(!find(lec[i].second)) continue;
+        merge(find(lec[i].second), find(lec[i].second) - 1);
+        ans += lec[i].first;
     }
     cout << ans << '\n';
 
