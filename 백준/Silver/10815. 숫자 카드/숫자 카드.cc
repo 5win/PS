cadd73b65344334ff5a0802034ce2ee618127b69
5win <94297900+5win@users.noreply.github.com>
2024-04-20 19:02:52 +0900

    [Silver V] Title: 숫자 카드, Time: 204 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -14,27 +14,39 @@ using namespace std;
 const int INF = 1987654321;
 const int MOD = 1000000007;
 
+int n, m;
+int arr[500001];
+
+bool binarySearch(int target) {
+    int lo = 0, hi = n - 1;
+
+    while(lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if(arr[mid] == target) return true;
+        else if(arr[mid] < target) lo = mid + 1;
+        else hi = mid - 1;
+    }
+    return false;
+}
 
 int main(void) {
     FASTIO;
 
-    int n, m, num;
-    map<int, int> card;
     cin >> n;
-    
-    while(n--) {
-        cin >> num;
-        card[num] = num;
+    int num;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
+    sort(arr, arr + n);
+
     cin >> m;
     while(m--) {
         cin >> num;
-        if(card.find(num) == card.end())
-            cout << "0 ";
-        else
+        if(binarySearch(num))
             cout << "1 ";
+        else
+            cout << "0 ";
     }
 
-    
     return 0;
 }
