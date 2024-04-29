2ea18e6a46ac5fb58acbb243c23d843e8069e8c1
5win <94297900+5win@users.noreply.github.com>
2024-04-29 21:39:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 284 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,32 @@
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+#include <math.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+
+const int INF = 1987654321;
+const int MOD = 1000000007;
+
+
+int main(void) {
+    FASTIO;
+
+    int n;
+    int arr[1000001];
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << '\n';
+
+    return 0;
+}
