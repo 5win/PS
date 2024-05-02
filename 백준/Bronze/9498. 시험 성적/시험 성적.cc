5b5cb6425a3a418c0f837a45f8e4e4501acbb37a
5win <94297900+5win@users.noreply.github.com>
2024-05-02 18:51:02 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    using namespace std;
+    
+    int a;
+    cin >> a;
+
+    if(90 <= a && a <= 100)
+        cout << 'A';
+    else if(80 <= a && a <= 89)
+        cout << 'B';
+    else if(70 <= a && a <= 79)
+        cout << 'C';
+    else if(60 <= a && a <= 69)
+        cout << 'D';
+    else
+        cout << 'F';
+
+    return 0;
+}
