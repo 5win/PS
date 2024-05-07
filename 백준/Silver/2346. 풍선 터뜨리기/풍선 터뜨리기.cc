faaf61e8776afd82ff770e1772dc982984dcc442
5win <94297900+5win@users.noreply.github.com>
2024-05-07 22:50:10 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -27,23 +27,21 @@ int main(void) {
         dq.push_back({input, i + 1});
     }
 
-    while(dq.size() > 1) {
+    while(!dq.empty()) {
         pair<int, int> cur = dq.front();
         dq.pop_front();
         cout << cur.second << ' ';
 
         int size = dq.size();
-        int cnt = cur.first < 0 ? abs(cur.first) : cur.first - 1;
-        cnt %= size;
-        // cout << cnt << ", ";
+        if(!size) break;
+
+        int cnt = (cur.first < 0) ? abs(cur.first) % size : (cur.first - 1) % size;
         cnt = (cur.first < 0) ? size - cnt : cnt;
-        // cout << "cnt : " << cnt << '\n';
         for(int i = 0; i < cnt; i++) {
             dq.push_back(dq.front());
             dq.pop_front();
         }
     }
-    cout << dq.front().second << '\n';
 
 
     return 0;
