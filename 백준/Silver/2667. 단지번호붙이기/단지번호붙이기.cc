a868c919a34d2d3d8800483fb987ecd7d70de7ea
5win <94297900+5win@users.noreply.github.com>
2024-05-17 21:41:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,72 @@
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+#include <math.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+const int INF = 1987654321;
+const int MOD = 1000000007;
+
+int n;
+string board[26];
+vector<int> res;
+
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
+
+bool inRange(int y, int x) {
+    return 0 <= y && y < n && 0 <= x && x < n;
+}
+
+void bfs() {
+    queue<pair<int, int>> q;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(board[i][j] == '1') {
+                int cnt = 1;
+                board[i][j] = '0';
+                q.push({i, j});
+                while(!q.empty()) {
+                    pair<int, int> cur = q.front();
+                    q.pop();
+                    
+                    int y = cur.first, x = cur.second;
+                    for(int dir = 0; dir < 4; dir++) {
+                        int ny = y + dy[dir], nx = x + dx[dir];
+                        if(!inRange(ny, nx) || board[ny][nx] == '0') continue;
+                        board[ny][nx] = '0';
+                        cnt++;
+                        q.push({ny, nx});
+                    }
+                }
+                res.push_back(cnt);
+            }
+        }
+    }
+}
+
+int main(void) {
+    FASTIO;
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> board[i];
+    }
+    bfs();
+    sort(res.begin(), res.end());
+    cout << res.size() << '\n';
+    for(auto i : res)
+        cout << i << '\n';
+
+    return 0;
+}
