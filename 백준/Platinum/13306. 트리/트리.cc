6d1c51f3a6052f3b40759f9a788876ae5b9d22f4
5win <94297900+5win@users.noreply.github.com>
2024-05-24 22:52:56 +0900

    [Platinum IV] Title: 트리, Time: 128 ms, Memory: 18356 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13306. 트리/트리.cc
+++ 백준/Platinum/13306. 트리/트리.cc
@@ -15,62 +15,66 @@ using namespace std;
 
 const int INF = 1987654321;
 const int MOD = 1000000007;
-int n, q;
-int parent[200001], level[200001];
-vector<int> adj[200001];
 
-bool lca(int u, int v) {
-	if(level[u] < level[v]) swap(u, v);
-	
-	while(level[u] != level[v]) {
-		if(u == parent[u]) return false;
-		u = parent[u];
-	}
-	
-	while(u != v) {
-		if(u == parent[u] || v == parent[v]) return false;
-		u = parent[u];
-		v = parent[v];
-	}
-	return u == v;
+struct Query {
+    int x, a, b;
+};
+
+int n, q;
+int parent[200001], tmpParent[200001];
+vector<Query> query;
+
+int find(int u) {
+    if(parent[u] == u) return u;
+    return parent[u] = find(parent[u]);
 }
 
-void setTree(int node, int pnode) {
-	parent[node] = pnode;
-	level[node] = level[pnode] + 1;
-	for(int i = 0; i < adj[node].size(); i++) {
-		int next = adj[node][i];
-		setTree(next, node);
-	}
+void merge(int u, int v) {
+    u = find(u); v = find(v);
+    if(u == v) return;
+    parent[v] = u;
 }
 
 int main(void) {
     FASTIO;
 
     cin >> n >> q;
+    for(int i =1; i <= n; i++)
+        parent[i] = i;
 	for(int i = 2; i <= n; i++) {
 		int p; cin >> p;
-		adj[p].push_back(i);
-		parent[i] = p;
+        tmpParent[i] = p;
 	}
-	setTree(1, 1);
 	
 	for(int i = 0; i < (n - 1) + q; i++) {
 		int x; cin >> x;
 		if(x == 0) {
 			int b; cin >> b;
-			parent[b] = b;
+            query.push_back({x, b});
 		} else {
 			int c, d;
 			cin >> c >> d;
-			if(lca(c, d)) {
-				cout << "YES\n";
-			} else {
-				cout << "NO\n";
-			}
+            query.push_back({x, c, d});
 		}
 	}
 
+    stack<string> res;
+    for(int i = query.size() - 1; i >= 0; i--) {
+        if(query[i].x == 0) {
+            int node = query[i].a;
+            merge(tmpParent[node], node);
+        } else {
+            int u = query[i].a, v = query[i].b;
+            if(find(u) == find(v))
+                res.push("YES\n");
+            else
+                res.push("NO\n");
+        }
+    }
+    while(!res.empty()) {
+        cout << res.top();
+        res.pop();
+    }
 
     return 0;
 }
