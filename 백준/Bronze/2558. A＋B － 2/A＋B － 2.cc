6355a01b5f2740bc4182761fbb4affad463cc484
5win <94297900+5win@users.noreply.github.com>
2024-05-28 23:26:17 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,27 @@
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+#include <math.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+const int INF = 1987654321;
+const int MOD = 1000000007;
+
+int main(void) {
+    FASTIO;
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+
+    return 0;
+}
