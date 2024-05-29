af17b2013140193e0380b4fb9c6d49356d0c6ad2
5win <94297900+5win@users.noreply.github.com>
2024-05-29 20:52:52 +0900

    [Gold III] Title: 파티, Time: 500 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,43 +1,60 @@
-#include <iostream>
+#include <vector>
+#include <queue>
+#include <string.h>
 #include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+#include <math.h>
 
-#define INF 987654321
 using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
 
-int n, m, arr[1001][1001], from, to, weight, target;
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1000000007;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+int n, m, x;
+int adj[1001][1001];
 
-	cin >> n >> m >> target;
+void floyd() {
+    for(int k = 1; k <= n; k++) {
+        for(int i = 1; i <= n; i++) {
+            if(adj[i][k] >= INF) continue;
+            for(int j = 1; j <= n; j++) {
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+            }
+        }
+    }
+}
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == j)
-				continue;
-			else
-				arr[i][j] = INF;
-		}
-	}
+int main(void) {
+    FASTIO;
 
-	for (int i = 0; i < m; i++) {
-		cin >> from >> to >> weight;
-		arr[from][to] = weight;
-	}
+    cin >> n >> m >> x;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            adj[i][j] = INF;
+            if(i == j) adj[i][j] = 0;
+        }
+    }
+        
+    for(int i = 0; i < m; i++) {
+        int s, e, t;
+        cin >> s >> e >> t;
+        adj[s][e] = t;
+    }
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (int k = 1; k <= n; k++) {
-				arr[j][k] = min(arr[j][k], arr[j][i] + arr[i][k]);
-			}
-		}
-	}
+    floyd();
 
-	int mx = 0;
+    int res = 0;
+    for(int i = 1; i <= n; i++) {
+        res = max(res, adj[i][x] + adj[x][i]);
+    }
+    cout << res << '\n';
 
-	for (int i = 1; i <= n; i++) {
-		mx = max(mx, arr[i][target] + arr[target][i]);
-	}
-	cout << mx;
+    return 0;
 }
