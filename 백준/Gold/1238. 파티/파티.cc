83106c0b8334611e510f84f0b28321ab92809205
5win <94297900+5win@users.noreply.github.com>
2024-05-29 21:16:35 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -18,41 +18,51 @@ const int INF = 1e9;
 const int MOD = 1000000007;
 
 int n, m, x;
-int adj[1001][1001];
+vector<pair<int, int>> adj[1001];
+vector<pair<int, int>> radj[1001];
 
-void floyd() {
-    for(int k = 1; k <= n; k++) {
-        for(int i = 1; i <= n; i++) {
-            if(adj[i][k] >= INF) continue;
-            for(int j = 1; j <= n; j++) {
-                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+vector<int> dijkstra(vector<pair<int, int>> curAdj[]) {
+    priority_queue<pair<int, int>> pq;
+    vector<int> dist(1001, INF);
+    dist[x] = 0;
+    pq.push({0, x});
+
+    while(!pq.empty()) {
+        pair<int, int> cur = pq.top();
+        pq.pop();
+        int cost = -cur.first;
+        int here = cur.second;
+        if(dist[here] < cost) continue;
+        for(int i = 0; i < curAdj[here].size(); i++) {
+            int next = curAdj[here][i].first;
+            int nextDist = cost + curAdj[here][i].second;
+            if(dist[next] > nextDist) {
+                dist[next] = nextDist;
+                pq.push({-nextDist, next});
             }
         }
     }
+    return dist;
 }
 
 int main(void) {
     FASTIO;
 
     cin >> n >> m >> x;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            adj[i][j] = INF;
-            if(i == j) adj[i][j] = 0;
-        }
-    }
         
     for(int i = 0; i < m; i++) {
         int s, e, t;
         cin >> s >> e >> t;
-        adj[s][e] = t;
+        adj[s].push_back({e, t});
+        radj[e].push_back({s, t});
     }
 
-    floyd();
+    vector<int> dist1 = dijkstra(adj);
+    vector<int> dist2 = dijkstra(radj);
 
     int res = 0;
     for(int i = 1; i <= n; i++) {
-        res = max(res, adj[i][x] + adj[x][i]);
+        res = max(res, dist1[i] + dist2[i]);
     }
     cout << res << '\n';
 
