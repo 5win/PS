fc534f224af2cc32504548e56c1f57f364448b5e
5win <94297900+5win@users.noreply.github.com>
2024-06-03 02:53:56 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,54 @@
+#include <vector>
+#include <queue>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+#include <map>
+#include <stack>
+#include <math.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1000000007;
+const int MAX = 1000001;
+
+int n;
+
+bool isGood(string s) {
+    stack<char> st;
+
+    for(const auto& c : s) {
+        if(c == 'A') {
+            if(!st.empty() && st.top() == 'A')
+                st.pop();
+            else 
+                st.push('A');
+        } else {
+            if(!st.empty() && st.top() == 'B')
+                st.pop();
+            else
+                st.push('B');
+        }
+    }
+    return st.empty();
+}
+
+int main(void) {
+    FASTIO;
+
+    cin >> n;
+    int cnt = 0;
+    while(n--) {
+        string s; cin >> s;
+        if(isGood(s)) cnt++;
+    }
+    cout << cnt << '\n';
+
+    return 0;
+}
