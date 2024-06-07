c15b7b92e2e64cc2f738a6ede2451988faaf31e0
5win <94297900+5win@users.noreply.github.com>
2024-06-07 23:26:39 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1000000007;
+const int MAX = 1000001;
+
+int main(void) {
+    FASTIO;
+
+    int n;
+    cin >> n;
+    cout << 1 << '\n' << 0 << '\n';
+
+    return 0;
+}
