078d00463101eb7ea0a55b4961b0c21e51ecc01c
5win <94297900+5win@users.noreply.github.com>
2024-06-15 16:45:40 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 196 ms, Memory: 88040 KB -BaekjoonHub

---
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e9;
+const int MAX = 40001;
+
+int n;
+int D[21][21];
+int dp[21][1 << 20];
+
+int dfs(int cur, int done) {
+    if(cur == n) {
+        return 0;
+    }
+
+    int &ret = dp[cur][done];
+    if(ret != -1) return ret;
+
+    int mn = INF;
+    for(int i = 0; i < n; i++) {
+        if(done & (1 << i)) continue;
+        mn = min(mn, dfs(cur + 1, done | (1 << i)) + D[cur][i]);
+    }
+    return ret = mn;
+}
+
+int main(void) {
+    FASTIO;
+
+    memset(dp, -1, sizeof(dp));
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> D[i][j];
+        }
+    }
+    cout << dfs(0, 0) << '\n';
+
+    return 0;
+}
