7cf0d66ec836e1be73ef208c1f91135b8a73f197
5win <94297900+5win@users.noreply.github.com>
2024-07-14 18:38:32 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e6;
+const int MAX = 40001;
+
+int n;
+bool dp[1001];
+
+int main(void) {
+    FASTIO;
+    
+    cin >> n;
+    dp[1] = true;
+    dp[2] = false;
+    dp[3] = true;
+    for(int i = 4; i <= n; i++) {
+        dp[i] = !(dp[i - 1] && dp[i - 3]);
+    }
+    if(dp[n])
+        cout << "SK";
+    else
+        cout << "CY";
+
+
+    return 0;
+}
