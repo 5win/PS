f90d62f7b0a8596e75eee50c1b9d11ac1bf00705
5win <94297900+5win@users.noreply.github.com>
2024-07-16 12:25:05 +0900

    [Silver II] Title: 좌표 압축, Time: 456 ms, Memory: 10152 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,19 +1,17 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <map>
-#include <algorithm>
-#include <string.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
 
-const int INF = 1987654321;
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e6;
+const int MAX = 40001;
 
-int n;
-vector<int> arr, res;
-map<int, int> m;
+int n, m;
+vector<int> X;
 
 int main(void) {
     FASTIO;
@@ -21,19 +19,18 @@ int main(void) {
     cin >> n;
     for(int i = 0; i < n; i++) {
         int tmp; cin >> tmp;
-        arr.push_back(tmp);
+        X.push_back(tmp);
     }
-    res = arr;
-
-    sort(arr.begin(), arr.end());
-    arr.erase(unique(arr.begin(), arr.end()), arr.end());
-
+    vector<int> sortedX(X);
     
-    for(int i = 0; i < res.size(); i++) {
-        auto it = lower_bound(arr.begin(), arr.end(), res[i]);
-        cout << it - arr.begin() << ' ';
+    // copy(X.begin(), X.end(), back_inserter(sortedX));
+    sort(sortedX.begin(), sortedX.end());
+    sortedX.erase(unique(sortedX.begin(), sortedX.end()), sortedX.end());
+
+    for(int i = 0; i < n; i++) {
+        cout << lower_bound(sortedX.begin(), sortedX.end(), X[i]) - sortedX.begin() << '\n';
     }
-    cout << '\n';
+
 
     return 0;
 }
