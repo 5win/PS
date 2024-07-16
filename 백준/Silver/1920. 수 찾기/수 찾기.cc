23372d318e253fa9ed698194611f39f373ca030f
5win <94297900+5win@users.noreply.github.com>
2024-07-16 11:32:24 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define LL long long
+#define ULL unsigned long long
+
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e6;
+const int MAX = 40001;
+
+int n, m;
+int A[100001];
+
+int main(void) {
+    FASTIO;
+    
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    sort(A, A + n);
+
+    cin >> m;
+    while(m--) {
+        int x; cin >> x;
+        if(binary_search(A, A + n, x)) {
+            cout << "1\n";
+        } else {
+            cout << "0\n";
+        }
+    }
+
+
+    return 0;
+}
