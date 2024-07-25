d7c792107e327622f459d5ce2a1057cf6f104dc7
5win <94297900+5win@users.noreply.github.com>
2024-07-25 14:18:01 +0900

    [Silver II] Title: 나무 자르기, Time: 156 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,39 +1,37 @@
-#include <vector>
-#include <queue>
-#include <string.h>
-#include <algorithm>
-#include <iostream>
-#include <climits>
-#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define LL long long
+#define ULL unsigned long long
 
-const int INF = 1987654321;
-const int MOD = 1000000007;
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e6;
+const int MAX = 40001;
 
 int n, m;
-vector<int> wood;
+int maxLen;
+int tree[1000001];
 
-bool solve(int h) {
-    long long sum = 0;
-    for(auto& i : wood) {
-        if(i > h)
-            sum += i - h;
+bool decision(int x) {
+    LL sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += (tree[i] >= x) ? tree[i] - x : 0; 
     }
-    return m <= sum;
+    return sum >= m;
 }
 
 int binarySearch() {
-    int lo = 0, hi = 1e9;
+    int lo = 0, hi = maxLen + 1;
 
     while(lo + 1 < hi) {
         int mid = (lo + hi) / 2;
-        if(solve(mid))
+        if(decision(mid)) {
             lo = mid;
-        else
+        } else {
             hi = mid;
+        }
     }
     return lo;
 }
@@ -43,10 +41,11 @@ int main(void) {
 
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
-        int tmp; cin >> tmp;
-        wood.push_back(tmp);
+        cin >> tree[i];
+        maxLen = max(maxLen, tree[i]);
     }
-    cout << binarySearch() << '\n';
 
+    cout << binarySearch() << '\n';
+    
     return 0;
 }
