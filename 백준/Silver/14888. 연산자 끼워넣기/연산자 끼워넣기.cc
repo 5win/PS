619148cfc683c510a82af26b03cf992ea1b31390
5win <94297900+5win@users.noreply.github.com>
2024-08-03 00:20:43 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,79 +1,74 @@
-#include <vector>
-#include <queue>
-#include <string.h>
-#include <algorithm>
-#include <iostream>
-#include <climits>
-#include <map>
-#include <stack>
-#include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define LL long long
 #define ULL unsigned long long
 
-const int INF = 1987654321;
-const int MOD = 1000000007;
+// const int INF = 1987654321;
+const int INF = 1e9;
+const int MOD = 1e6;
+const int MAX = 40001;
 
 int n;
-int nums[15];
-int op[5];
+int A[12];
+int opers[4];
+int mn = INF, mx = -INF;
 
-int minVal = INF, maxVal = -INF;
-vector<int> seq;
-
-void calcMinMax() {
-    int res = nums[0];
-    for(int i = 1; i < n; i++) {
-        switch(seq[i - 1]) {
-            case 0:
-                res += nums[i];
-                break;
-            case 1:
-                res -= nums[i];
-                break;
-            case 2:
-                res *= nums[i];
-                break;
-            case 3:
-                res /= nums[i];
-                break;
-        }
-    }
-    minVal = min(minVal, res);
-    maxVal = max(maxVal, res);
+int calculate(vector<int>& expr) {
+	int res = A[0];
+	for(int i = 1; i < expr.size(); i++) {
+		switch(expr[i]) {
+			case 0:
+				res += A[i];
+				break;
+			case 1:
+				res -= A[i];
+				break;
+			case 2:
+				res *= A[i];
+				break;
+			case 3:
+				res /= A[i];
+				break;
+		}
+	}
+	return res;
 }
 
-void dfs(int curCnt) {
-    if(curCnt == n - 1) {
-        calcMinMax(); 
-        return;
-    }
+void dfs(int cur, vector<int>& expr) {
+	if(cur == n - 1) {
+		int res = calculate(expr);
+		mn = min(mn, res);
+		mx = max(mx, res);
+		return;
+	}
 
-    for(int i = 0; i < 4; i++) {
-        if(op[i] > 0) {
-            op[i]--;
-            seq.push_back(i);
-            dfs(curCnt + 1);
-            seq.pop_back();
-            op[i]++;
-        }
-    }
+	for(int i = 0; i < 4; i++) {
+		if(opers[i] != 0) {
+			opers[i]--;
+			expr.push_back(i);
+			dfs(cur + 1, expr);
+			expr.pop_back();
+			opers[i]++;
+		}
+	}
 }
 
 int main(void) {
     FASTIO;
 
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    for(int i = 0; i < 4; i++) {
-        cin >> op[i];
-    }
-    dfs(0);
-    cout << maxVal << '\n' << minVal << '\n';
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+	for(int i = 0; i < 4; i++)
+		cin >> opers[i];
+
+	vector<int> v;
+	v.push_back(-1);
+	dfs(0, v);
+	cout << mx << '\n' << mn << '\n';
 
     return 0;
 }
