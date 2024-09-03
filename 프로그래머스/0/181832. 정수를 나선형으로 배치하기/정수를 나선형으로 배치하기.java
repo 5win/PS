39f95fc58feabe1824ea9d99a0ae0cd26077980a
5win <94297900+5win@users.noreply.github.com>
2024-09-03 22:02:57 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.20 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -3,18 +3,13 @@ import java.util.*;
 class Solution {
     int[] dy = {0, 1, 0, -1};
     int[] dx = {1, 0, -1, 0};
-    int N;
     
-    boolean inRange(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < N;
+    boolean inRange(int y, int x, int n) {
+        return 0 <= y && y < n && 0 <= x && x < n;
     }
     
     public int[][] solution(int n) {
-        N = n;
         int[][] answer = new int[n][n];
-        for(int i = 0; i < n; i++) {
-        	Arrays.fill(answer[i], 0);
-        }
        
        	int y = 0, x = 0;
         int dir = 0;
@@ -22,7 +17,7 @@ class Solution {
             answer[y][x] = i;
             
             int ny = y + dy[dir], nx = x + dx[dir];
-            if(!inRange(ny, nx) || answer[ny][nx] != 0) {
+            if(!inRange(ny, nx, n) || answer[ny][nx] != 0) {
                	dir = (dir + 1) % 4; 
             }
             y += dy[dir];
