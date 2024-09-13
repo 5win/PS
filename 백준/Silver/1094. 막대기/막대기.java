de2064fa1bdcb472fc139d257dee8e6e5ab8fff4
5win <94297900+5win@users.noreply.github.com>
2024-09-13 22:04:58 +0900

    [Silver V] Title: 막대기, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -2,31 +2,16 @@ import java.util.*;
 
 public class Main {
     static int x;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-    static void solve() {
-        pq.offer(64);
-        int lenSum = 64;
-
-        while(lenSum != x) {
-            int minVal = pq.poll();
-            int num = minVal / 2;
-
-            pq.offer(num);
-            if(lenSum - num >= x) {
-                lenSum -= num;
-                continue;
-            }
-            pq.offer(num);
-        }
-    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
 
-        solve();
-        System.out.println(pq.size());
+        int cnt = 0;
+        for(int i = 0; i <= 6; i++) {
+            if((x & (1 << i)) > 0) cnt++;
+        }
+        System.out.println(cnt);
     }
 }
