d710a4a1265e2adce49915cb8dc543cf43c207e1
5win <94297900+5win@users.noreply.github.com>
2024-10-01 20:49:56 +0900

    [Gold V] Title: 용액, Time: 324 ms, Memory: 30532 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -5,6 +5,27 @@ import java.util.stream.IntStream;
 import java.io.*;
 
 public class Main {
+    static int[] arr = new int[100001];
+    static int sum = Integer.MAX_VALUE;
+    static int[] res = {0, 0};
+
+    static void binarySearch(int s, int e, int left) {
+        while(s <= e) {
+            int mid = (s + e) / 2;
+            int ab = Math.abs(arr[left] + arr[mid]);
+
+            if(sum > ab) {
+                sum = ab;
+                res[0] = arr[left]; res[1] = arr[mid];
+            }
+
+            if(arr[mid] + arr[left] <= 0) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,22 +33,12 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        int[] arr = new int[100001];
         for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(input[i]);
 
-        int s = 0, e = n - 1;
-        int[] res = {arr[s], arr[e]};
-        int sum = Math.abs(arr[s] + arr[e]);
-
-        while(s < e) {
-            int cur = arr[s] + arr[e];
-            if(Math.abs(cur) < sum) {
-                sum = Math.abs(cur);
-                res[0] = arr[s]; res[1] = arr[e];
-            }
-            if(cur <= 0) s++;
-            else e--;
+        for(int i = 0; i < n; i++) {
+            binarySearch(i + 1, n - 1, i);
         }
+
         bw.write(String.valueOf(res[0]) + " " + String.valueOf(res[1]));
 
         bw.flush();
