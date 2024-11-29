2200f4b8e60a4eb7386ce99bad99eaa92b9b77a0
5win <94297900+5win@users.noreply.github.com>
2024-11-29 14:54:54 +0900

    [Silver I] Title: 종이 접기, Time: 112 ms, Memory: 15116 KB -BaekjoonHub

---
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.io.*;
+
+public class Main {
+
+    static int t;
+    static String str;
+
+    static boolean check(int l, int mid, int r) {
+        int left = mid - 1, right = mid + 1;
+        while(l <= left && right <= r) {
+            if(str.charAt(left) == str.charAt(right)) return false;
+            left--;
+            right++;
+        }
+        return true;
+    }
+
+    static boolean solve(int l, int r) {
+
+        if(l >= r) return true;
+
+        int mid = (l + r) / 2;
+        String left = str.substring(l, mid);
+        String right = str.substring(mid + 1, r + 1);
+
+        if(!check(l, mid, r)) return false;
+
+        boolean ret = true;
+        ret = ret && solve(l, mid - 1) && solve(mid + 1, r);
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0) {
+            str = br.readLine();
+            if(solve(0, str.length() - 1))
+                bw.write("YES\n");
+            else
+                bw.write("NO\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
