e5596a3ebdc02066c0ec9276eb40217559adfdc3
5win <94297900+5win@users.noreply.github.com>
2024-11-29 12:25:07 +0900

    [Platinum III] Title: 문자열 장식, Time: 276 ms, Memory: 30600 KB -BaekjoonHub

---
+++ 백준/Platinum/1294. 문자열 장식/문자열 장식.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static String[] str;
+
+    static String solve() {
+        PriorityQueue<String> pq = new PriorityQueue<>((String a, String b) -> {
+            if(a.length() <= b.length() && b.startsWith(a))
+                return 1;
+            else if(a.length() > b.length() && a.startsWith(b))
+                return -1;
+            return a.compareTo(b);
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            pq.offer(str[i]);
+        }
+
+        while(!pq.isEmpty()) {
+            String here = pq.poll();
+            
+            sb.append(here.charAt(0));
+
+            if(here.length() == 1) continue;
+            String next = here.substring(1);
+            pq.offer(next);
+        }
+
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        str = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            str[i] = br.readLine();
+        }
+
+        bw.write(solve() + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
