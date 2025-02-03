e558ef48801992046f7dc64bc60d99952b30864d
5win <94297900+5win@users.noreply.github.com>
2025-02-03 18:19:40 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 13.51 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=java#) 
 
 ### 성능 요약
 
-메모리: 89.5 MB, 시간: 13.00 ms
+메모리: 73 MB, 시간: 13.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 03일 18:08:41
+2025년 02월 03일 18:19:38
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,58 +1,32 @@
 class Solution {
+    public int convertToSec(String time) {
+   		String[] ms = time.split(":");
+        return Integer.parseInt(ms[0]) * 60 + Integer.parseInt(ms[1]);
+    }
+    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         String answer = "";
         
-        String[] videoTime = video_len.split(":");
-        String[] posTime = pos.split(":");
-        String[] opStart = op_start.split(":");
-        String[] opEnd = op_end.split(":");
-            
-        int vm = Integer.parseInt(videoTime[0]);
-        int vs = Integer.parseInt(videoTime[1]);
-        int pm = Integer.parseInt(posTime[0]);
-        int ps = Integer.parseInt(posTime[1]);
-        int osm = Integer.parseInt(opStart[0]);
-        int oss = Integer.parseInt(opStart[1]);
-        int oem = Integer.parseInt(opEnd[0]);
-        int oes = Integer.parseInt(opEnd[1]);
+        int vt = convertToSec(video_len);
+        int pt = convertToSec(pos);
+        int os = convertToSec(op_start);
+        int oe = convertToSec(op_end);
            
         for(String cmd : commands) {
-            if((osm < pm || (osm == pm && oss <= ps)) && (pm < oem || (pm == oem && ps <= oes))) {
-                pm = oem;
-                ps = oes;
-            }
+        	if(os <= pt && pt <= oe) {
+                pt = oe;
+            }    
             
             if(cmd.equals("prev")) {
-                if(pm == 0 && ps < 10) {
-                    ps = 0;
-                } else if(pm > 0 && ps < 10) {
-                    pm--;
-                    ps = 60 - (10 - ps);
-                } else {
-                    ps -= 10;
-                }
+               	pt = Math.max(0, pt - 10); 
             } else {
-                if(pm == vm && vs - ps < 10) {
-                    ps = vs; 
-                } else if(pm < vm && ps >= 50) {
-                    pm++;
-                    if(pm == vm) {
-                        ps = Math.min(vs, ps - 50);
-                    } else {
-                    	ps = ps - 50;
-                    }
-                } else {
-                    ps += 10;
-                }
+               	pt = Math.min(vt, pt + 10); 
             }
         }
-        if((osm < pm || (osm == pm && oss <= ps)) && (pm < oem || (pm == oem && ps <= oes))) {
-            pm = oem;
-            ps = oes;
-        }
+        if(os <= pt && pt <= oe) {
+            pt = oe;
+        }    
         
-        answer = String.format("%02d", pm) + ":" + String.format("%02d", ps);
-        
-        return answer;
+        return String.format("%02d", pt / 60) + ":" + String.format("%02d", pt % 60);
     }
 }
