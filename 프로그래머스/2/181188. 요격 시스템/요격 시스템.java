8b2ae0976161e7a325009c54584a441bc246941c
5win <94297900+5win@users.noreply.github.com>
2025-02-05 11:51:38 +0900

    [level 2] Title: 요격 시스템, Time: 448.35 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,32 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         
-        List<int[]> list = new ArrayList<>();
-        for(int i = 0; i < targets.length; i++) {
-            list.add(new int[]{targets[i][0], 1, i});
-            list.add(new int[]{targets[i][1], -1, i});
-        }
-        Collections.sort(list, (a, b) -> {
-           	if(a[0] == b[0]) {
-                return a[1] - b[1];
+        Arrays.sort(targets, (a, b) -> {
+           	if(a[1] == b[1]) {
+                return a[0] - b[0];
             } 
-            return a[0] - b[0];
+            return a[1] - b[1];
         });
         
         int res = 0;
-        int curSize = 0;
-        Set<Integer> willDelete = new HashSet<>();
-        Set<Integer> deleted = new HashSet<>();
-        for(int i = 0; i < list.size(); i++) {
-       		int[] here = list.get(i);
-            if(here[1] == -1 && !deleted.contains(here[2])) {
-                res++;
-                deleted.addAll(willDelete);
-                willDelete.clear();
-            } else {
-                willDelete.add(here[2]);
-            }
+        int lastEnd = targets[0][1];
+        for(int i = 1; i < targets.length; i++) {
+       		int[] here = targets[i];
+           	if(here[0] >= lastEnd) {
+                System.out.println(here[0]);
+               	res++;
+                lastEnd = here[1];
+            } 
         }
+        res++;
+        
         return res;
     }
 }
