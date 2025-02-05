7793b6dcc0a9cb0546317a88fd39303ab7abdde6
5win <94297900+5win@users.noreply.github.com>
2025-02-05 10:52:19 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 8.91 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,34 +1,21 @@
 import java.util.*;
 
 class Solution {
+	List<String> exts;
+    
     public int convertExt(String ext) {
-       	if(ext.equals("code")) {
-            return 0;
-        } else if(ext.equals("date")) {
-           	return 1; 
-        } else if(ext.equals("maximum")) {
-           	return 2; 
-        }
-        return 3; 
+        return exts.indexOf(ext);
     }
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        String[] extArray = {"code", "date", "maximum", "remain"};
+       	exts = Arrays.asList(extArray);
         
-        List<int[]> filtered = new ArrayList<>();
         int target = convertExt(ext);
-        int idx = 0;
-        for(int[] d : data) {
-           	if(d[target] < val_ext) {
-                filtered.add(d);
-            }
-        }
-        
         int sortBy = convertExt(sort_by);
-        Collections.sort(filtered, (a, b) -> a[sortBy] - b[sortBy]);
-       	
-        int[][] res = new int[filtered.size()][4];
-        for(int i = 0; i < filtered.size(); i++) {
-            res[i] = filtered.get(i);
-        }
-        return res;
+        
+        return Arrays.stream(data)
+            .filter(d -> d[target] < val_ext)
+            .sorted((a, b) -> a[sortBy] - b[sortBy])
+            .toArray(int[][]::new);
     }
 }
